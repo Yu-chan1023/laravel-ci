c28cbc7c41580380410861b3e21af4d7c39b2037
senkyo-minami <y.minami@senkyo.co>
2021-10-26 15:50:07 +0900

    deploy-test

--- resources/views/nav.blade.php
+++ resources/views/nav.blade.php
@@ -6,7 +6,7 @@
 
     @guest
     <li class="nav-item">
-      <a class="nav-link" href="{{ route('register') }}">ユ-ザ-登録</a>
+      <a class="nav-link" href="{{ route('register') }}">ユーザー登録</a>
     </li>
     @endguest
 
